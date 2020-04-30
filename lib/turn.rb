1f7560a29e70e8e61d2244c72d6e61210270f5ef
Andrew Trent <andrewtrentdesign@gmail.com>
2020-04-30 03:43:05 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def display_board(board)
+  puts board
+end
+
+def input_to_index(input)
+  return input -= 1
+end
+
+def valid_move?(move_pos)
+
+end
+
+def move(board, move_pos, character)
+
+end
+
+def turn()
+
+end
